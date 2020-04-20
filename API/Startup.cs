19a01ace0dcc61aed7d3ce1e669ea0a47383e398
Michael John Rieser <michi.rieser@gmail.com>
2020-04-20 16:49:48 +0200

    End of Section 4: CRUD using CQRS + Mediator pattern

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace API
@@ -42,6 +37,9 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 });
             });
+
+            // Add support for Mediatr
+            services.AddMediatR(typeof(List.Handler).Assembly);
 
             // Make it possible to use web api controllers
             services.AddControllers();
